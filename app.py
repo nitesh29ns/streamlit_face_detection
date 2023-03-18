7b16a6a7aa54e57141afea521e7ee0bae4729e9d
nitesh29ns <94296741+nitesh29ns@users.noreply.github.com>
2023-03-18 23:26:34 +0530

    add video_transformer_factory

--- app.py
+++ app.py
@@ -1,40 +1,59 @@
+# using video_transformer_factory
 
 import cv2
 import numpy as np
 from PIL import Image
 import av
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration, VideoTransformerBase
+import threading
+from typing import Union
 import torch
+from PIL import Image
+
 
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
-#st.model = torch.hub.load('./yolov5/', 'custom', path='my_face_detection_model.pt', source='local')
-if not hasattr(st, 'classifier'):
-    st.model = torch.hub.load('ultralytics/yolov5', 'yolov5s',  _verbose=False)
+def main():
+    if st.button("yolov5s"):
+        st.model = torch.hub.load('ultralytics/yolov5', 'yolov5s',  _verbose=False)
 
-class VideoProcessor:
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        # img = process(img)
+    if st.button("custom_model"):
+        st.model = torch.hub.load('./yolov5/', 'custom', path='my_face_detection_model.pt', source='local')
 
-         # vision processing
-        flipped = img[:, ::-1, :]
 
-        # model processing
-        im_pil = Image.fromarray(flipped)
-        results = st.model(im_pil)
-        bbox_img = np.array(results.render()[0])
+    class VideoTransformer(VideoTransformerBase):
+        frame_lock: threading.Lock  # `transform()` is running in another thread, then a lock object is used here for thread-safety.
+        in_image: Union[np.ndarray, None]
+        out_image: Union[np.ndarray, None]
 
-        return av.VideoFrame.from_ndarray(bbox_img, format="bgr24")
-    
-webrtc_ctx = webrtc_streamer(
-    key="WYH",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration=RTC_CONFIGURATION,
-    media_stream_constraints={"video": True, "audio": False},
-    video_processor_factory=VideoProcessor,
-    async_processing=True,
-)
+        def __init__(self) -> None:
+            self.frame_lock = threading.Lock()
+            self.in_image = None
+            self.out_image = None
+
+        def transform(self, frame: av.VideoFrame) -> np.ndarray:
+            in_image = frame.to_ndarray(format="bgr24")
+
+            out_image = in_image[:, ::-1, :]  # Simple flipping for example.
+
+            im_pil = Image.fromarray(out_image)
+            results = st.model(im_pil)
+            bbox_img = np.array(results.render()[0])
+
+            #return av.VideoFrame.from_ndarray(bbox_img, format="bgr24")
+            return bbox_img
+        
+    webrtc_ctx = webrtc_streamer(
+        key="WYH",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
+        video_transformer_factory=VideoTransformer
+    )
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,43 @@
+# using  video_processor_factory
+import cv2
+import numpy as np
+from PIL import Image
+import av
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+import torch
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+#st.model = torch.hub.load('./yolov5/', 'custom', path='my_face_detection_model.pt', source='local')
+if st.button("yolov5s"):
+    st.model = torch.hub.load('ultralytics/yolov5', 'yolov5s',  _verbose=False)
+
+if st.button("custom_model"):
+    st.model = torch.hub.load('./yolov5/', 'custom', path='my_face_detection_model.pt', source='local')
+
+class VideoProcessor:
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        # img = process(img)
+
+         # vision processing
+        flipped = img[:, ::-1, :]
+
+        # model processing
+        im_pil = Image.fromarray(flipped)
+        results = st.model(im_pil)
+        bbox_img = np.array(results.render()[0])
+
+        return av.VideoFrame.from_ndarray(bbox_img, format="bgr24")
+    
+webrtc_ctx = webrtc_streamer(
+    key="WYH",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={"video": True, "audio": False},
+    video_processor_factory=VideoProcessor,
+    async_processing=True,
+)
