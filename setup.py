b17e388d1a695e27fe643b74d443182e32ecda01
nitesh29ns <94296741+nitesh29ns@users.noreply.github.com>
2023-03-20 20:28:05 +0530

    add setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from typing import List
+
+#declaring variables for setup funcations
+PROJECT_NAME= "face-detection"
+VERSION="0.0.1"
+AUTHOR="Nitesh Sharma"
+DESCRIPTION="my face detection yolov5 model"
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+
+def get_requirements_list()->List[str]: # List[str] used to declare the structure of the output.
+    """
+    description : this funcation is going to return the list of requirements mention in the requirements.txt file
+
+    return_type :List[str]
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirements_file:
+        return requirements_file.readlines().remove("-e .") 
+          
+          
+# .remove("-e .") beacuse we use find_packages()
+
+
+setup(
+name =PROJECT_NAME,
+version =VERSION,
+author =AUTHOR,
+description =DESCRIPTION,
+packages =find_packages(),  #read code we write in the __init__.py, where it has __init__.py it read the file, -e . is eaual to find_pakages()
+install_requires =get_requirements_list()
+)
